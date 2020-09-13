b05c73428184bd1eab77be986fee49a8f0bb59bc
Hans Larsen <hans@larsen.online>
2020-09-12 18:44:39 -0700

    test: remove invalid flag

--- src/cbor/src/deserialize/peek/simple_test.rs
+++ src/cbor/src/deserialize/peek/simple_test.rs
@@ -1,19 +1,19 @@
 #![cfg(all(feature = "std", feature = "half"))]
 use crate::deserialize::peek;
-use crate::serialize::values;
+use crate::serialize::owned;
 use crate::test_utils::assert_peek;
 
 #[test]
 fn simple() {
-    assert_peek(values::r#true(), "f5", peek::r#true);
-    assert_peek(values::r#false(), "f4", peek::r#false);
-    assert_peek(values::null(), "f6", peek::null);
-    assert_peek(values::undefined(), "f7", peek::undefined);
+    assert_peek(owned::r#true(), "f5", peek::r#true);
+    assert_peek(owned::r#false(), "f4", peek::r#false);
+    assert_peek(owned::null(), "f6", peek::null);
+    assert_peek(owned::undefined(), "f7", peek::undefined);
 }
 
 #[test]
 fn floats() {
-    assert_peek(values::half_float_from_f32(1.2), "f93ccd", peek::f16);
-    assert_peek(values::f32(4.5), "fa40900000", peek::f32);
-    assert_peek(values::f64(6.7), "fb401acccccccccccd", peek::f64);
+    assert_peek(owned::half_float_from_f32(1.2), "f93ccd", peek::f16);
+    assert_peek(owned::f32(4.5), "fa40900000", peek::f32);
+    assert_peek(owned::f64(6.7), "fb401acccccccccccd", peek::f64);
 }
